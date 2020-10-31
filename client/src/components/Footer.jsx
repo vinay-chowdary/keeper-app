0e5db711d95476c27d741bc29cba2879ce856a10
vinay-chowdary <vinaychowdary.dev@gmail.com>
2020-10-31 21:38:31 +0530

    add Header and Footer components

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Footer = () => (
+    <footer>
+        <p>Copyright &copy; {new Date().getFullYear()} </p>
+    </footer>
+);
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => (
+    <header >
+        <h1 className="container">
+            <i class="far fa-lightbulb"></i>
+            <a href="/" className="brand">Keeper</a>
+        </h1>
+    </header>
+);
