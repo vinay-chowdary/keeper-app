b104f4cceddc0c55f9532aa10536dab95b7f9ce3
vinay-chowdary <vinaychowdary.dev@gmail.com>
2021-01-12 15:46:19 +0530

    changed icon - logo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Logo from '@material-ui/icons/WbIncandescentOutlined';
 
 export const Header = () => (
     <header >
         <h1 className="container">
-            <i class="far fa-lightbulb"></i>
+            <Logo fontSize="large" className="logo" />
             <a href="/" className="brand">Keeper</a>
         </h1>
     </header>
