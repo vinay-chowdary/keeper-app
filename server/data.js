d72bb8817b04aad44a1986bb22ee17e0392370c3
vinay-chowdary <vinaychowdary.dev@gmail.com>
2021-02-04 14:10:10 +0530

    connect mongodb atlas and test working with sample data

---
+++ server/data.js
@@ -0,0 +1,60 @@
+const notes = [
+    {
+        title: "title 1",
+        content: "content 1 content 1 content 1 content 1 content 1 content 1 content 1 content 1 content 1"
+    },
+    {
+        title: "title 2 title 2 title 2 title 2",
+        content: "content 2 content 2 content 2 content 2 content 2 content 2 content 2 content 2"
+    },
+    {
+        title: "title 3 title 3 ",
+        content: "content 3 content 3 content 3"
+    },
+    {
+        title: "title 4",
+        content: "content 4"
+    },
+    {
+        title: "title 5 title 5 title 5 title 5",
+        content: "content 5 content 5 content 5 content 5 content 5 content 5 content 5 content 5 content 5 "
+    },
+    {
+        title: "title 6",
+        content: "content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6"
+    },
+    {
+        title: "title 6",
+        content: "content 6 content 6 content 6"
+    }, {
+        title: "title 1",
+        content: "content 1 content 1 content 1 content 1 content 1 content 1 content 1 content 1 content 1"
+    },
+    {
+        title: "title 2 title 2 title 2 title 2",
+        content: "content 2 content 2 content 2 content 2 content 2 content 2 content 2 content 2"
+    },
+    {
+        title: "title 3 title 3 ",
+        content: "content 3 content 3 content 3"
+    },
+    {
+        title: "title 4",
+        content: "content 4"
+    },
+    {
+        title: "title 5 title 5 title 5 title 5",
+        content: "content 5 content 5 content 5 content 5 content 5 content 5 content 5 content 5 content 5 "
+    },
+    {
+        title: "title 6",
+        content: "content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6 content 6"
+    },
+    {
+        title: "title 6",
+        content: "content 6 content 6 content 6"
+    }
+]
+
+
+module.exports = notes
--- server/server.js
+++ server/server.js
@@ -1,14 +1,57 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const data = require('./data')
+require('dotenv').config()
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("client/build"));
+// app.use(express.static("client/build"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server started at ${port}`));
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/client/build/index.html");
+
+
+
+
+
+
+
+
+
+const dbURL = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@cluster-vinay.dfnbs.mongodb.net/${process.env.DB_NAME}`;
+
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const noteSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+const documents = data;
+
+Note.insertMany(documents).then((err) => {
 })
+
+
+// note.save()
+
+
+
+app.get("/api/notes", async (req, res) => {
+
+    try {
+        const notes = await Note.find({})
+        res.send(JSON.stringify(notes))
+    }
+
+    catch (err) {
+        console.log(err);
+    }
+
+
+})
+
